c351bc45b3fca14cc55b4c3cc857c5c1ff7a2e2c
Alexis <contact@alexis-petit.fr>
2026-04-23 14:14:24 +0200

    traefik

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome <a href="https://nextjs.org">CESI!</a>
+          sjdkjdqkfj <a href="https://nextjs.org">CESI!</a>
         </h1>
 
         <p className={styles.description}>
